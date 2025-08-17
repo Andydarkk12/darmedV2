2a8abb7a7063895ec50621e15e2cfe1dc17c0a30
Andydarkk <darkhanovandrey@gmail.com>
2025-08-17 21:26:19 +0700

    first commit

---
+++ server/server.js
@@ -0,0 +1,48 @@
+const TelegramBot = require('node-telegram-bot-api');
+const cors = require("cors")
+const token = "8441908209:AAGV-6-L9ujDSbpYYBV3JKsohjuuBgwHN1g"
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.use(cors());
+app.use(express.json());
+
+const bot = new TelegramBot(token, {
+    polling: true
+});
+
+const example = {
+    type:'УЗИ',
+    date:'завтра',
+    name:'Андрей',
+    phone:'880005553535',
+    comment:'что то',
+}
+
+const parseObject = (object) =>{
+    return ` <b>Вид исследования:</b> ${object.type}\n\n<b>Предпочтительная дата:</b>  ${object.date}\n\n<b>Фамилия Имя Очество:</b>  ${object.name}\n\n<b>Номер телефона:</b>  ${object.phone}\n\n<b>Комментарий:</b>  ${object.comment}\n`
+}
+
+const sendMessage = async (data) =>{
+    await bot.sendMessage(1644953534, `<b>Новая заявка на сайте!</b>\n\n`+parseObject(data),{parse_mode: 'HTML'});
+}
+bot.on('message', (msg) => {
+    console.log('Новое сообщение от пользователя:', msg);
+});
+
+
+app.post('/', async (req, res) => {
+    try {
+        const formData = req.body; // данные из React
+        await sendMessage(req.body);
+        res.json({ success: true });
+    } catch (err) {
+        console.error('Ошибка при отправке сообщения:', err);
+        res.status(500).json({ success: false, error: err.message });
+    }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
